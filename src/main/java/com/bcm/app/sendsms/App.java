2fb386ce4382c6b75a56c52105a07b3d76874ae6
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-01-18 15:55:11 +0800

    Add the init runnable version

---
+++ src/main/java/com/bcm/app/sendsms/App.java
@@ -0,0 +1,13 @@
+package com.bcm.app.sendsms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
