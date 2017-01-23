72334b231e6f0a5d268afda7794a9c2bbc089f3b
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-01-23 14:58:22 +0800

    Move old half translated code to App.java.bak

--- src/main/java/com/bcm/app/sendsms/App.java
+++ src/main/java/com/bcm/app/sendsms/App.java
@@ -1,13 +1,11 @@
 package com.bcm.app.sendsms;
 
 /**
- * Hello world!
+ * 
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main( String[] args ){
+      System.out.println("Spring io");
     }
 }
