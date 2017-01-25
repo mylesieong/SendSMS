6be4a9838a52a4b6a9541a8ec9bf641df08c4c7b
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-01-25 18:23:27 +0800

    Add and test the upload script

--- src/main/java/com/bcm/app/sendsms/ScheduledTasks.java
+++ src/main/java/com/bcm/app/sendsms/ScheduledTasks.java
@@ -9,8 +9,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
@@ -39,7 +41,7 @@ public class ScheduledTasks {
                 return ;
             }
             boolean success = ftpClient.login(user, pass);
-            /* Try to download the fileA.txt and SWIFT/fileB.txt */
+            /* Download the fileA.txt and SWIFT/fileB.txt */
             String workingDirectory = System.getProperty("user.dir");
             OutputStream output;
             
@@ -52,8 +54,16 @@ public class ScheduledTasks {
             ftpClient.retrieveFile("fileB.txt", output);
             
             output.close();
-            /*/Try to download the fileA.txt and SWIFT/fileB.txt */
+            /*/Download the fileA.txt and SWIFT/fileB.txt */
             
+            /* Upload the test.txt at SendSMS folder */
+            InputStream input;
+            input = new FileInputStream(workingDirectory + "\\test.txt");
+            ftpClient.appendFile("test_upload", input);
+            input.close();
+            
+            /* /Upload the test.txt at SendSMS folder */
+ 
             showServerReply(ftpClient);
             if (!success) {
                 System.out.println("Could not login to the server");
